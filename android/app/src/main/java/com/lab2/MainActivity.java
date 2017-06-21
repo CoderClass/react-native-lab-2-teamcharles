e2bd22198275d87ea0ddf144ec3dd2e1db102075
Charles <chug2k@gmail.com>
2017-06-21 22:50:46 +0700

    Milestone 2 just setup

--- android/app/src/main/java/com/lab2/MainActivity.java
+++ android/app/src/main/java/com/lab2/MainActivity.java
@@ -1,6 +1,8 @@
 package com.lab2;
 
 import com.facebook.react.ReactActivity;
+import android.content.Intent;
+
 
 public class MainActivity extends ReactActivity {
 
@@ -11,5 +13,10 @@ public class MainActivity extends ReactActivity {
     @Override
     protected String getMainComponentName() {
         return "lab2";
+    }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        MainApplication.getCallbackManager().onActivityResult(requestCode, resultCode, data);
     }
 }
